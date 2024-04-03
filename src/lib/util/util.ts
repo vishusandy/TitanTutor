eb9afaedd72000c364fe6708bf59eacc4c80aaa1
Andrew <kaioshin2005@yahoo.com>
2024-04-03 17:08:08 -0500

    removed formatDuration from util/util.ts (use util/lang.ts instead)

--- src/lib/util/util.ts
+++ src/lib/util/util.ts
@@ -12,23 +12,6 @@ export function shuffle(input: string[]): string[] {
     return array;
 };
 
-// https://lingohub.com/blog/pluralization
-// modified slightly (for multilingual support) from:
-// https://www.30secondsofcode.org/js/s/format-duration/
-export const formatDuration = (ms: number, lang: Language) => {
-    if (ms < 0) ms = -ms;
-    const time = {
-        day: Math.floor(ms / 86400000),
-        hour: Math.floor(ms / 3600000) % 24,
-        minute: Math.floor(ms / 60000) % 60,
-        second: Math.floor(ms / 1000) % 60,
-    };
-    return Object.entries(time)
-        .filter(val => val[1] !== 0 || val[0] == 'second')
-        .map(([key, val]) => `${val} ` + (val !== 1 ? lang[key + 's'] : lang[key]))
-        .join(', ');
-};
-
 export function defaultBatch(lesson: Lesson, n: number) {
     let words: string[] = [];
 
